ddecae50b6ea795529bac5b663d67952cfba130e
Tony Chen <tonychen@Tonys-MacBook-Pro.local>
2022-11-19 00:07:25 -0800

    first commit

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import discord
+from discord.ext import commands
 
+#initialize the bot with the commands imported from discord.ext and use '!' as command prefix to call the bot
+client=commands.Bot(command_prefix='!')
 
-if __name__ == '__main__':
-    print_hi('Hello World')
+# async is a special type of function, on_ready is also a function that it will execute automatically when it is ready to receive command
+@client.event
+async def on_ready():
+    print("I am still alive!!")
+    print("-------------------")
+    
