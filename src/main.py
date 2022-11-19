22a65aaf4408dbde475dffe2096d4a0bba2b166a
Tony Chen <tonychen@Tonys-MacBook-Pro.local>
2022-11-19 02:25:00 -0800

    token and some basics

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
+from config import token
 import discord
-from discord.ext import commands
+intents = discord.Intents.all()
+intents.typing = False
+intents.presences = False
 
+
+from discord.ext import commands
 #initialize the bot with the commands imported from discord.ext and use '!' as command prefix to call the bot
-client=commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='!', intents=intents)
 
 # async is a special type of function, on_ready is also a function that it will execute automatically when it is ready to receive command
 @client.event
 async def on_ready():
     print("I am still alive!!")
     print("-------------------")
-    
+
+#define our own funciton here i just type in hello first, ctx is like taking inputs from disord, so the user will type in !hello to excecute the coomand in discord
+@client.command()
+async def hello(ctx):
+    await ctx.send("Welcome to Google Student Developer Club@GRC/competitive programming club!")
+
+#token -> never want to reveal this
+client.run(token)
