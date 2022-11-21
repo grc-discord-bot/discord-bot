c37224fbc0ca96442c8f16b424d180df86a95a96
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-21 09:42:36 -0800

    updated

---
+++ src/bot.py
@@ -0,0 +1,64 @@
+import discord
+from discord.ext import commands
+import responses
+from dotenv import load_dotenv
+import os
+
+
+# Configure the token key
+def configure():
+    load_dotenv()
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    configure()
+    token = os.getenv('token_key')
+
+    intents = discord.Intents.all()
+    intents.members = True
+    intents.message_content = True
+    intents.typing = False
+    intents.presences = False
+    client = commands.Bot(command_prefix='!', intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        # begin a private conversation with the bot initiated by a question mark
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    @client.event
+    async def on_member_join(member):
+        print(f'{member} has joined the server.')
+
+    @client.event
+    async def on_member_remove(member):
+        channel = client.get_channel(1044095575282425906)
+        await channel.send(f'{member} Hello and welcome to the server!')
+
+    client.run(token)
--- src/main.py
+++ src/main.py
@@ -1,41 +1,4 @@
-from config import token
-import discord
-intents = discord.Intents.all()
-intents.members=True
-intents.typing = False
-intents.presences = False
+from src import bot
 
-
-from discord.ext import commands
-#initialize the bot with the commands imported from discord.ext and use '!' as command prefix to call the bot
-client = commands.Bot(command_prefix='!', intents=intents)
-
-# async is a special type of function, on_ready is also a function that it will execute automatically when it is ready to receive command
-#event is when encounter with certain condition, it will run certain code
-@client.event
-async def on_ready():
-    print("I am still alive!!")
-    print("-------------------")
-
-#define our own funciton here i just type in hello first, ctx is like taking inputs from disord, so the user will type in !hello to excecute the coomand in discord
-@client.command()
-async def hello(ctx):
-    await ctx.send("Welcome to Google Student Developer Club@GRC/competitive programming club!")
-
-@client.event
-async def on_member_join(memebr):
-    channel= client.get_channel(1044095575282425906)
-    await channel.send("hello and welcome to our club server")
-    #these two lines below were just trying out something 
-    #await channel.send(f"{member.mention} hello and welcome to our club server'")
-    #f"{member} welcome to the server!'
-
-#not a really useful event since the user who left can't see this message
-#@client.event
-#async def on_member_remove(memebr):
-#    channel= client.get_channel(1044095575282425906)
-#    await channel.send("so sad you leave, but you are always welcome to come back! although you can't see this:(")
-
-#token -> never want to reveal this!!
-client.run(token)
-
+if __name__ == '__main__':
+    bot.run_discord_bot()
