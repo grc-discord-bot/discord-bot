bbbcb70fe976f2eaba39f79bc1fad0614d49a2b8
Tony Chen <tonychen@Tonys-MacBook-Pro.local>
2022-11-21 23:55:26 -0800

    uncomment the help command

--- src/responses.py
+++ src/responses.py
@@ -7,7 +7,7 @@ def get_response(message: str) -> str:
     if process_msg == 'hello':
         return 'Welcome to Google Student Developer Club@GRC/competitive programming club!'
 
-#    if process_msg == '!help':
-#        return "`This is a help message that you can modify.`"
+    if process_msg == '$help':
+        return "`This is a help message that you can modify.`"
 
 #    return "I don't understand what you are saying."
