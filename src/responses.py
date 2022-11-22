be5308f6b77d24d636229b5f3c529bead92cd273
Tony Chen <tonychen@Tonys-MacBook-Pro.local>
2022-11-22 00:29:52 -0800

    changes in the response sentence

--- src/responses.py
+++ src/responses.py
@@ -5,7 +5,7 @@ def get_response(message: str) -> str:
     process_msg = message.lower()
 
     if process_msg == 'hello':
-        return 'Welcome to Google Student Developer Club@GRC/competitive programming club!'
+        return 'Welcome to Google Student Developer Club@GRC/competitive programming club! I am Mardo the moderator bot for this club, click on my icon to see how to interact with me!'
 
     if process_msg == '$help':
         return "`This is a help message that you can modify.`"
