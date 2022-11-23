4e06ea8f48c210aeb81984fd194d82fdda168b66
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-22 17:04:04 -0800

    updated help message

--- src/responses.py
+++ src/responses.py
@@ -10,9 +10,9 @@ def get_response(message: str) -> str:
     if process_msg == '$help':
         return """
         `You can type in the commands below in the channel #bot-commands to interact with Mardo:`
-        1. $help -> which will make this current message appear
-        2. hello -> which will give you a welcome message
-        3. ? -> to start a private message with Mardo
+        `1. $help -> which will make this current message appear`
+        `2. hello -> which will give you a welcome message`
+        `3. ? -> to start a private message with Mardo`
         """
 
 #    return "I don't understand what you are saying."
