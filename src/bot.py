799dd6ad4f138fb8b435089bbb2d814b3ae8020c
Tony Chen <tonychen@Tonys-MacBook-Pro.local>
2022-11-22 16:11:04 -0800

    changes in the help command in response.py

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,24 @@ import responses
 from dotenv import load_dotenv
 import os
 
+#class CustomHelpCommand(commands.HelpCommand):
+#    def __init__(self):
+#        super().__init__
+
+#    async def send_bot_help(self, mapping):
+#        for cog in mapping:
+#            await self.get_destination().send(f'{cog.quilified_name}: {[command.name for command in mapping[cog]]}')
+
+#    async def send_cog_help(self, cog: Cog, /) -> None:
+#        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]}')
+
+#    async def send_group_help(self, group):
+#        await self.get_destination().send(f'{group.name}: {[command.name for index, command in enumerate(group.commands)]}')
+
+#    async def send_command_help(self, command):
+#        await self.get_destination().send(command.name)
+
+    
 
 # Configure the token key
 def configure():
@@ -28,7 +46,7 @@ def run_discord_bot():
     intents.message_content = True
     intents.typing = False
     intents.presences = False
-    client = commands.Bot(command_prefix='$', intents=intents)
+    client = commands.Bot(command_prefix='$', intents=intents, help_command=CustomHelpCommand)
 
     @client.event
     async def on_ready():
