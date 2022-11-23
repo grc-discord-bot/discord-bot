30e0181b61885c6e52bb51584e92b0dd8caf922a
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-22 16:49:53 -0800

    updated

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
 import responses
-#from env import load_dotenv
+# from env import load_dotenv
 from dotenv import load_dotenv
 import os
 
-#class CustomHelpCommand(commands.HelpCommand):
+
+# class CustomHelpCommand(commands.HelpCommand):
 #    def __init__(self):
 #        super().__init__
 
@@ -16,13 +17,12 @@ import os
 #    async def send_cog_help(self, cog: Cog, /) -> None:
 #        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]}')
 
-#    async def send_group_help(self, group):
-#        await self.get_destination().send(f'{group.name}: {[command.name for index, command in enumerate(group.commands)]}')
+# async def send_group_help(self, group): await self.get_destination().send(f'{group.name}: {[command.name for index,
+# command in enumerate(group.commands)]}')
 
 #    async def send_command_help(self, command):
 #        await self.get_destination().send(command.name)
 
-    
 
 # Configure the token key
 def configure():
