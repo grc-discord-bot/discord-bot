c16e459602e7986e1c5aef6ca7542f4ad2cc56a3
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2022-11-22 16:54:59 -0800

    updated

--- src/responses.py
+++ src/responses.py
@@ -8,8 +8,9 @@ def get_response(message: str) -> str:
                'bot for this club, click on my icon to see how to interact with me! '
 
     if process_msg == '$help':
-        return "`You can type in the commands below in the channel #bot-commands to interact with Mardo: 1. $help -> " \
-               "which will make this current message appear` 2. hello -> which will give you a welcome message 3. ? " \
-               "-> to start a private message with Mardo "
+        return "`You can type in the commands below in the channel #bot-commands to interact with Mardo:" \
+               " 1. $help -> " "which will make this current message appear` " \
+               " 2. hello -> which will give you a welcome message 3. " \
+               " ? " "-> to start a private message with Mardo "
 
 #    return "I don't understand what you are saying."
