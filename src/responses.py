fa8009d63a4a4824fe941f1183031413a0953770
Hazel Zhou <hazelzhou@Hazels-MacBook-Pro.local>
2023-01-17 14:41:34 -0800

    I edited the responses in 'responses.py'. Added a few links and club info

--- src/responses.py
+++ src/responses.py
@@ -4,7 +4,7 @@ def get_response(message: str) -> str:
     process_msg = message.lower()
 
     if process_msg == 'hello':
-        return 'Welcome to Google Student Developer Club@GRC/competitive programming club! I am Mardo the moderator ' \
+        return 'Welcome to Google Developer Student Club @GRC / Competitive Programming Club! I am Mardo the moderator ' \
                'bot for this club, click on my icon to see how to interact with me! '
 
     if process_msg == '$help':
@@ -22,27 +22,31 @@ def get_response(message: str) -> str:
     if process_msg == '$resources':
         return """
         Here are some useful resources for programming and tech:
-        - [Insert resource 1]
-        - [Insert resource 2]
-        - [Insert resource 3]
+        - [Tony's tutorial](https://common-paste-ea7.notion.site/Content-Topics-1c3b68e1d8894f8b99564fc5efd6836b)
+        - [HackerRank](https://www.hackerrank.com/)
         """
 
     if process_msg == '$events':
         return """
         Here is a list of all upcoming events:
-        - [Insert event 1] (Date: [Insert date])
-        - [Insert event 2] (Date: [Insert date])
-        - [Insert event 3] (Date: [Insert date])
+        - First Meeting (in-person) 
+        Date: Wednesday Jan 25
+        Salish Hall 152
         """
 
     if process_msg == '$join':
         return """
-        Thank you for your interest in joining our club! Please fill out this form to become a member: [Insert link to form]
+        Thank you for your interest in joining our club! Please fill out this form to become a member: https://forms.gle/tXvs3t5quB9q3cHF7
         """
 
     if process_msg == '$contact':
         return """
-        If you have any questions or would like to contact us, please email us at [Insert email address]. We will get back to you as soon as possible.
+        If you have any questions or would like to contact us, please dm our discord or ask in the club server. We will get back to you as soon as possible.
+        """
+    
+    if process_msg == '$website':
+        return """
+        This is our club website, you can find more info there! https://sites.google.com/view/gdscpclub/home
         """
 
 #    return "I don't understand what you are saying."
