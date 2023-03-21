2a2edab553a3c3acdc1bafdb67db18c52928c0df
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-03-20 20:16:39 -0700

    Delete ChatGPT folder

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from src.discord_bot.bot import bot, discord_token
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+bot.run(os.getenv("DISCORD_TOKEN"))
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix="!")
 
 
-def main():
-    bot.run(discord_token)
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
 
 
-if __name__ == '__main__':
-    main()
+for filename in os.listdir("./src/discord_bot/cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"src.discord_bot.cogs.{filename[:-3]}")
+
+bot = discord.Client(intents=intents)
